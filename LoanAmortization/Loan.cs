eff442245ea08edda078bf6ab96c58f777859782
jarenmontano <47220484+jarenmontano@users.noreply.github.com>
2024-05-06 12:16:58 -0400

    Add project files.

---
+++ LoanAmortization/Loan.cs
@@ -0,0 +1,138 @@
+﻿/* Loan.cs
+ * Purpose: this class will hold all of the data about a loan
+ * it will also be able to calculate things based off of the
+ * loan amout, term, and rate.
+ * Author: Jaren Montano
+ * Date: 9/30/2023
+ */
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanAmortization
+{
+    internal class Loan
+    {
+        private int numPayments;
+        private double balance;
+        private double monthInterest;
+        private double principal;
+
+        public double PaymentAmount { get; set; }
+        public double LoanAmount { get; set; }
+        public double Rate { get; set; }
+        public double TotalInterestPaid { get; set; }
+
+        public int Years {
+
+            get
+            {
+                return numPayments/12;
+            }
+            set
+            {
+                numPayments = value * 12;
+            }
+        }
+
+        public Loan(double loan, double interestRate, int years)
+        {
+            LoanAmount = loan;
+            if (interestRate < 1) {
+                Rate = interestRate;
+            }
+            else//error handling / convert to decimal
+            {
+                Rate = interestRate / 100;
+            }
+
+            numPayments = 12 * years;
+            TotalInterestPaid = 0;
+            DeterminePaymentAmount();
+            
+            
+        }
+
+        //Determine the payment amount based on number of
+        //Years, Loan Amount, and Rate
+        private void DeterminePaymentAmount()
+        {
+            double term;
+            term = Math.Pow((1 + Rate / 12), numPayments);
+            PaymentAmount = (LoanAmount * Rate / 12.0 * term) / (term - 1.0);
+        }
+
+        //return information about the loan
+        public override string ToString()
+        {
+            return "\nLoan Amount: " + LoanAmount.ToString("C") +
+                "\nInterest Rate: " + Rate +
+                "\nNumber of Years for Loan: " +
+                (numPayments / 12) +
+                "\nMonthly payment: " + PaymentAmount.ToString("C");
+        }
+
+        //the purpose is to return string containing amoritization table
+        public string ReturnAmortizationSchedule()
+        {
+
+            string schedule = "Month\t\tInt.\t\tPrin.\t\tNew" +
+                "\nNo.\t\tPd.\t\tPd.\t\tBalance\n";
+            schedule += "_____\t\t_____\t\t_____\t\t_____\n";
+
+            balance = LoanAmount; 
+
+            for(int month = 1; month <= numPayments; month++)
+            {
+                CalculateMonthCharges(month, numPayments);
+                schedule += month + "\t" + monthInterest.ToString("N2").PadLeft(13)
+                    + "\t" + principal.ToString("N2").PadLeft(15) + "\t" +
+                    balance.ToString("C").PadLeft(17) + "\n";
+            }
+
+            return schedule;
+        }
+
+
+        //calculate the monthly interest and new balance for each month
+        private void CalculateMonthCharges(int month, int numPayments)
+        {
+            double payment = PaymentAmount;
+            monthInterest = Rate/ 12  * balance;
+
+            if (month == numPayments)
+            {
+                principal = balance;
+                payment = balance + monthInterest;
+            }
+            else
+            {
+                principal= payment - monthInterest;
+            }
+
+            balance -= principal;
+        }
+
+        //calculates the interest paid over life of the loan
+        public void DetermineTotalInterestPaid()
+        {
+            TotalInterestPaid = 0;
+            balance = LoanAmount;
+
+            for(int month = 1;month <= numPayments; month++)
+            {
+                CalculateMonthCharges(month, numPayments);
+                TotalInterestPaid += monthInterest;
+            }
+
+        }
+
+    }
+}
---
+++ LoanAmortization/LoanApp.cs
@@ -0,0 +1,102 @@
+﻿/* LoanApp.cs
+ * Purpose: The purpose of this application is to get data from the user
+ * then connect it to the class loan,
+ * then will print all of the info from the class loan.cs
+ * Author: Jaren Montano
+ * Date: 9/30/2023
+ * 
+ */
+
+
+
+using System;
+using System.Dynamic;
+using static System.Console;
+
+namespace LoanAmortization 
+{
+    internal class LoanApp
+    {
+        static void Main(string[] args)
+        {
+
+            int years;
+            double loanAmount;
+            double interestRate;
+            string inValue;
+            char anotherLoan = 'N';
+
+            do
+            {
+                GetInputValues(out loanAmount, out interestRate, out years);
+                Loan ln = new Loan(loanAmount, interestRate, years);
+                Clear();
+                WriteLine(ln);
+                WriteLine();
+                WriteLine(ln.ReturnAmortizationSchedule()); 
+                ln.DetermineTotalInterestPaid(); 
+                WriteLine("Payment Amount: {0:C}", ln.PaymentAmount);
+                WriteLine("Interest Paid over life of loan: {0:C}", ln.TotalInterestPaid);
+                WriteLine("Do another Calculation? (Y or N)");
+                inValue = ReadLine();
+                anotherLoan = Convert.ToChar(inValue);
+
+            }
+            while ((anotherLoan == 'Y') || (anotherLoan == 'y'));
+        }
+
+        private static void GetInputValues(out double loanAmount, out double interestRate, out int years)
+        {
+            Clear();
+            loanAmount = GetLoanAmount();
+            interestRate = GetInterestRate();
+            years = GetYears();
+
+        }
+
+        private static int GetYears()
+        {
+            string sValue;
+            int years;
+            Write("Please Enter the number of years for the loan:  ");
+            sValue = ReadLine();
+            while ((int.TryParse(sValue, out years) == false) || years < 1 || years > 30)
+            {
+                WriteLine("Invalid Data Entered " + "for years.");
+                Write("\nPlease re-enter years (1-30 only):  ");
+                sValue = ReadLine();
+            }
+            return years;
+        }
+
+        private static double GetInterestRate()
+        {
+            string sValue;
+            double interestRate;
+            Write("Please Enter a interest rate (as a decimal value - i.e. 0.06):  ");
+            sValue = ReadLine();
+            while ((double.TryParse(sValue, out interestRate) == false) || interestRate < 0 || interestRate > 1)
+            {
+                WriteLine("Invalid Data Entered " + "for interest rate.");
+                Write("\nPlease re-enter a decimal value (like 0.06):  ");
+                sValue = ReadLine();
+            }
+            return interestRate;
+        }
+
+        private static double GetLoanAmount()
+        {
+            string sValue;
+            double loanAmount;
+            Write("Please Enter a loan amount: ");
+            sValue = ReadLine();
+            while ((double.TryParse(sValue, out loanAmount) == false) || loanAmount < 1 || loanAmount > 500000)
+            {
+                WriteLine("Invalid Data Entered " + "for loan amount.");
+                Write("\nPlease re-enter the loan amount (less than $500,000):  ");
+                sValue = ReadLine();
+            }
+            return loanAmount;
+        }
+    }
+} 
